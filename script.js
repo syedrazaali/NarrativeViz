dcdcd06a32a0bcaa4db9c6d0f404e935dfadd673
Raza  Ali <111824503+syedrazaali@users.noreply.github.com>
2023-07-22 17:41:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-// Load the data from the CSV file
-d3.csv("players.csv").then(data => {
-    console.log(data);  // Log the data for debugging
+// Load the data
+d3.csv("players.csv").then(function(data) {
+  // Process the data to get the count of players for each position
+  var positionData = d3.rollups(data, v => v.length, d => d.position);
+  positionData = Array.from(positionData, d => ({position: d[0], count: d[1]}));
+  
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(positionData.map(function(d) { return d.position; }))
+    .padding(0.2);
+  // rest of the x-axis code...
+  
+  // Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(positionData, function(d) { return d.count; })])
+    .range([ height, 0]);
+  // rest of the y-axis code...
 
-    // Process the data (calculate averages, etc.)
+  // Bars
+  svg.selectAll("mybar")
+    .data(positionData)
+    .enter()
+    .append("rect")
+    // rest of the bar code...
 
-    // Create the visualization
-}).catch(error => {
-    console.log(error);  // Log any error that occurred
-});
+  // Animation
+  svg.selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("y", function(d) { return y(d.count); })
+    .attr("height", function(d) { return height - y(d.count); })
+    .delay(function(d,i){return(i*100)})
+
+}).catch(function(error){
+  console.log(error);
+})
