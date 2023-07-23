c7de0be754af8df0ae97493a452a43a85dfa9c10
Raza  Ali <111824503+syedrazaali@users.noreply.github.com>
2023-07-22 18:13:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+// define the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 600 - margin.left - margin.right, // Adjust the width here
+    height = 400 - margin.top - margin.bottom; // Adjust the height here
 
-// Append the svg object to the div called 'chart'
+// append the svg object to the body of the page
 var svg = d3.select("#chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -12,51 +12,96 @@ var svg = d3.select("#chart")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Load the data
+// read data
 d3.csv("players.csv").then(function(data) {
-  // Process the data to get the count of players for each position
-  var positionData = d3.rollups(data, v => v.length, d => d.position);
-  positionData = Array.from(positionData, d => ({position: d[0], count: d[1]}));
+
+  // group the data: I want to count the number of players per position
+  var positionCount = d3.rollup(data, v => v.length, d => d.position);
+
+  // create an array from the map
+  var positionArray = Array.from(positionCount, d => ({position: d[0], count: d[1]}));
 
   // X axis
   var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(positionData.map(function(d) { return d.position; }))
+    .range([0, width])
+    .domain(positionArray.map(function(d) { return d.position; }))
     .padding(0.2);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+    .call(d3.axisBottom(x));
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, d3.max(positionData, function(d) { return d.count; })])
-    .range([ height, 0]);
+    .domain([0, d3.max(positionArray, d => d.count)])
+    .range([height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
   // Bars
-  svg.selectAll("mybar")
-    .data(positionData)
+  var bars = svg.selectAll("mybar")
+    .data(positionArray)
     .enter()
     .append("rect")
-    .attr("x", function(d) { return x(d.position); })
-    .attr("width", x.bandwidth())
-    .attr("fill", "#69b3a2")
-    // no bar at the beginning thus:
-    .attr("height", function(d) { return height - y(0); }) // always equal to 0
-    .attr("y", function(d) { return y(0); }) // always equal to height
+      .attr("x", function(d) { return x(d.position); })
+      .attr("y", function(d) { return y(d.count); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.count); })
+      .attr("fill", "#69b3a2");
 
-  // Animation
-  svg.selectAll("rect")
-    .transition()
-    .duration(800)
-    .attr("y", function(d) { return y(d.count); })
-    .attr("height", function(d) { return height - y(d.count); })
-    .delay(function(d,i){return(i*100)})
+  // X axis label
+  svg.append("text")
+    .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
+    .style("text-anchor", "middle")
+    .text("Position");
 
-}).catch(function(error){
-  console.log(error);
-})
+  // Y axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Count");
+
+  // Title
+  svg.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "16px") 
+    .style("text-decoration", "underline")  
+    .text("Distribution of Players by Position");
+
+  // Tooltip
+  var tooltip = d3.select("#chart")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px");
+
+ var mouseover = function(d) {
+  tooltip
+    .style("opacity", 1);
+};
+
+var mousemove = function(d) {
+  tooltip
+    .html("Position: " + d.position + "<br>Count: " + d.count)
+    .style("left", (d3.mouse(this)[0]+70) + "px")
+    .style("top", (d3.mouse(this)[1]) + "px");
+};
+
+var mouseleave = function(d) {
+  tooltip
+    .style("opacity", 0);
+};
+
+bars
+  .on("mouseover", mouseover)
+  .on("mousemove", mousemove)
+  .on("mouseleave", mouseleave);
+});
