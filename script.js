7a6c566771ded9236ba9591585a27d5e9165435a
Raza  Ali <111824503+syedrazaali@users.noreply.github.com>
2023-07-25 00:07:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ var margin = {top: 30, right: 30, bottom: 70, left: 80},
     width = 770 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
-    // var color = d3.scaleThreshold()
-    //  .domain([25, 30, 40])
-    //  .range(["#f94144", "#f3722c", "#f9c74f", "#90be6d", "#43aa8b", "#577590"]);
-
     function createScatterPlot(chartID, data, xAxisProp, yAxisProp, colorProp, chartTitle) {
+        document.getElementById('chartTitle').innerText = "Player Attribute Breakdown";
     var svg = d3.select(chartID)
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -86,16 +83,16 @@ var margin = {top: 30, right: 30, bottom: 70, left: 80},
         .attr("x", -margin.top)
         .text(yAxisProp);
 
-    // Legend
-    var ageRanges = ["20-25", "25-30", "30-35", "35-40", "40+"];
+// Legend
+var ageRanges = ["20-25", "25-30", "30-35", "35-40", "40+"];
 
 var legend = svg.selectAll(".legend")
     .data(ageRanges)
     .enter().append("g")
     .attr("class", "legend")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    .attr("transform", function(d, i) { return "translate(0," + (height - i * 20 - 25) + ")"; }); // Adjust this for legend position
 
-    legend.append("rect")
+legend.append("rect")
     .attr("x", width - 18)
     .attr("width", 18)
     .attr("height", 18)
@@ -107,6 +104,7 @@ legend.append("text")
     .attr("dy", ".35em")
     .style("text-anchor", "end")
     .text(function(d) { return d; });
+    
 }
 // Function to filter data based on position
 function filterDataByPosition(data, position) {
@@ -117,6 +115,7 @@ function filterDataByPosition(data, position) {
 function createBarChart(chartID, data, property, chartTitle) {
     // Append the SVG object to the chart div of the page,
     // and set the dimensions of this SVG
+    document.getElementById('chartTitle').innerText = chartTitle;
     var svg = d3.select(chartID)
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -281,7 +280,7 @@ positionButton.addEventListener('click', function() {
         data = csvData; // set global data variable
 
         let stats = calculateStats(data, 'position');
-        document.getElementById('scene1Message').innerText = `There are a total of ${stats.total} players. The position with the most players is ${stats.mostProperty} with ${stats.highestValue} players, while the position with the least players is ${stats.leastProperty} with ${stats.lowestValue} players.`;
+        document.getElementById('scene1Message').innerText = `Our visualization journey begins with an overview of the total number of NFL players, clocking in at an impressive ${stats.total}. The Player Count by Position bar chart provides us with a bird's eye view of the league, highlighting the distribution of players across the various roles. The chart instantly captures our attention towards the position brimming with the highest count of players - ${stats.mostProperty}, boasting a whopping ${stats.highestValue} players. This might not come as a surprise considering the pivotal role this position plays in the dynamics of the game. However, the position that lays claim to the fewest players is ${stats.leastProperty}, fielding merely ${stats.lowestValue} players. This disparity in the count of players might hint at the relative complexity or niche requirements of this role, but can also be broken down into a subset of the runningback position.`;
 
         clearChart();
         createBarChart("#chart", data, "position", "Player Count by Position");
@@ -302,7 +301,7 @@ ageButton.addEventListener('click', function() {
         data.forEach(d => d.age = currentYear - d.birthDate.getFullYear());
 
         let stats = calculateStats(data, 'averageAge');
-        document.getElementById('scene1Message').innerText = `Age distribution gives us a clear representation of the athletic profile that breaks down each position, specifically giving us insight as to the hardest positions there are to be played. The position with the oldest average age is ${stats.mostProperty} at ${stats.highestValue} demonstrating precision outweighs athletic profile, while the position with the youngest average age is ${stats.leastProperty} at ${stats.lowestValue} which is argued amongst analysts to be the most difficult and taxing position to be played.`;
+        document.getElementById('scene1Message').innerText = `The Average Age by Position bar chart paints a fascinating picture of the relationship between the player's age and their position within the NFL. This age distribution is not merely numbers; it offers valuable insights into the athletic profile required for each position and the physical demands they entail. With a quick glance at the chart, it's possible to identify which positions are considered the most challenging or taxing to play. For instance, the position with the oldest average age, ${stats.mostProperty}, with an average age of ${stats.highestValue}, suggests that experience and tactical acumen often outshine sheer athleticism. On the other hand, the position with the youngest average age, ${stats.leastProperty}, at a tender ${stats.lowestValue}, is frequently viewed as the most physically challenging and taxing to be played. This dichotomy underscores the complexity and diversity of skills and physical prowess required across different positions in the NFL. Not only does the chart represent the demographic landscape of the league, but it also prompts us to appreciate the nuances and intricacies that make the sport so engaging and unpredictable.`;
 
         clearChart();
         createBarChart("#chart", data, "averageAge", "Average Age of Players by Position");
@@ -364,9 +363,52 @@ scatterPlotButton.addEventListener('click', function() {
         let filteredData = filterDataByPosition(data, selectedPosition);
 
         // call the scatter plot function with the required arguments
+        document.getElementById('scene1Message').innerText = "The scatterplot represents a comprehensive breakdown of player attributes in terms of height and weight, color-coded by age. This graphical representation offers insights into the typical physical profiles across different positions in the NFL, with each dot representing an individual player and includes the functionality to be able to zoom in and out of the graph to get a closer look at each position. By color-coding the data points by age, the plot provides an additional layer of information, offering a glimpse into how age distributions can vary across positions. This visualization is particularly useful for identifying patterns and correlations between the physical attributes and age of players. The drop-down menu allows for data filtering, enabling viewers to focus on specific positions for more detailed analysis. As seen in the plot, each position is vastly different: age, height, and weight demographics all vary widely from the more Athletic positions in comparison to the blocking and rushing positions. WR, RB, CB cover the most athletic positions in the NFL which is why we see a younger age, taller height, and moderate weight for these positions. This dataset clearly breaks down the mold of NFL player by position.   ";
         clearChart();
         createScatterPlot("#chart", filteredData, "height", "weight", "age", "Player Height vs Weight colored by Age");
     });
 });
+
+// get reference to all buttons with class 'tablinks'
+let tabButtons = document.querySelectorAll('.tablinks');
+
+// add event listener to each button
+tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        setActiveTab(button);
+    });
+});
+
+// function to set active tab
+function setActiveTab(activeButton) {
+    // remove active class from all buttons
+    tabButtons.forEach(button => {
+        button.classList.remove('active');
+    });
+    // add active class to the active button
+    activeButton.classList.add('active');
+}
+
+var hasVisitedPosition = false;
+var hasVisitedAge = false;
+
+document.getElementById('positionButton').addEventListener('click', function() {
+  hasVisitedPosition = true;
+  checkAccessToFullBreakdown();
+});
+
+document.getElementById('ageButton').addEventListener('click', function() {
+  hasVisitedAge = true;
+  checkAccessToFullBreakdown();
+});
+
+function checkAccessToFullBreakdown() {
+  if (hasVisitedPosition && hasVisitedAge) {
+    document.getElementById('scatterPlotButton').disabled = false;
+  }
+}
+
+// Initially disable the full breakdown tab
+document.getElementById('scatterPlotButton').disabled = true;
 
 positionButton.click()
